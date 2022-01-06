0a95374805f55a975805a8a3f646cdc5f92ca15d
thz <thz>
2022-01-06 11:52:58 +0800

    test4

---
+++ python/learn5.py
@@ -0,0 +1,18 @@
+class Dog:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def sit(self):
+        print(self.name)
+
+    def roll(self):
+        print(self.age, self.name)
+
+
+dog = Dog('willie', 6)
+
+print(dog.name)
+print(dog.age)
+dog.roll()
+dog.sit()
