38d8caf1374df684a05f97323fadb0d9aff02be2
thz <thz>
2022-01-08 11:21:16 +0800

    项目1.1

---
+++ alien/settings.py
@@ -0,0 +1,13 @@
+class Settings:
+    def __init__(self):
+        self.screen_width = 1000
+        self.screen_height = 600
+        self.screen_width2 = 1000
+        self.screen_height2 = 600
+        self.bg_color = (230, 230, 230)
+        self.ship_speed = 1.5
+        self.bullet_speed = 1.0
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_color = (60, 60, 60)
+        self.bullets_allowed = 3
