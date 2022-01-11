a46f412388fa57d624766be5beba9f8d63b5d70c
thz <thz>
2022-01-11 09:27:52 +0800

    项目2.0.3

--- matplotlib/eq_world_map.py
+++ matplotlib/eq_world_map.py
@@ -16,19 +16,26 @@ print(magl[:10])
 data = pd.DataFrame(data=zip(eq.lons, eq.lats, eq.titles, eq.mags), columns=["经度", "纬度", "位置", "震级"])
 data.head()
 
-fig = px.scatter(
+fig = px.scatter_mapbox(
     data,
-    x="经度",
-    y="纬度",
-    range_x=[-200, 200],
-    range_y=[-90, 90],
+    lon="经度",
+    lat="纬度",
+    # range_x=[-200, 200],
+    # range_y=[-90, 90],
     width=800,
     height=800,
     title="全球地震散点图",
     size="震级",
     size_max=11,
     color="震级",
+    hover_name="位置",
+    zoom=0,
+    center={"lat": 37, "lon": 101}
 )
+fig.update_layout(mapbox={'accesstoken': 'pk.eyJ1IjoidGh6aHViIiwiYSI6ImNreTlleDh1ZjA1cTgydm55cTE5ZHc4N2QifQ.sW3YhTZLDUEzq0t8ZgziHg', 'style': 'basic'},
+                  title=dict(x=0.5, xref='paper'),
+                  margin={'l': 10, "r": 100, 't': 10, 'b': 0}
+                  )
 
 fig.write_html("data/global_earthquakes.html")
 fig.show()
