036509f7a78a8ea2a2d5ae14f49e120ce11beb79
thz <thz>
2022-01-13 12:27:52 +0800

    项目3.0.2

--- web/learning_log/learning_logs/urls.py
+++ web/learning_log/learning_logs/urls.py
@@ -8,6 +8,9 @@ app_name = 'learning_logs'
 urlpatterns = [
     # Home page
     path('', views.index, name='index'),
-
+    path('topics/', views.topics, name='topics'),
+    path('topics/<int:topic_id>/', views.topic, name='topic'),
+    path('new_topic/', views.new_topic, name='new_topic'),
+    path('new_entry/<int:topic_id>/', views.new_entry, name='new_entry'),
 
 ]
--- web/learning_log/learning_logs/views.py
+++ web/learning_log/learning_logs/views.py
@@ -1,4 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+
+from .models import Topic
+from .forms import TopicForm, EntryForm
 
 
 # Create your views here.
@@ -6,3 +9,49 @@ from django.shortcuts import render
 def index(request):
     """学习笔记的主页。"""
     return render(request, 'learning_logs/index.html')
+
+
+def topics(request):
+    topics = Topic.objects.order_by('date_added')
+    context = {'topics': topics}
+    return render(request, 'learning_logs/topics.html', context)
+
+
+def topic(request, topic_id):
+    topic = Topic.objects.get(id=topic_id)
+    entries = topic.entry_set.order_by('-date_added')
+    context = {'topic': topic, 'entries': entries}
+    return render(request, 'learning_logs/topic.html', context)
+
+
+def new_topic(request):
+    if request.method != 'POST':
+        form = TopicForm()
+    else:
+        form = TopicForm(data=request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('learning_logs:topics')
+
+    context = {'form': form}
+    return render(request, 'learning_logs/new_topic.html', context)
+
+
+def new_entry(request, topic_id):
+    """Add a new entry for a particular topic."""
+    topic = Topic.objects.get(id=topic_id)
+
+    if request.method != 'POST':
+        # No data submitted; create a blank form.
+        form = EntryForm()
+    else:
+        # POST data submitted; process data.
+        form = EntryForm(data=request.POST)
+        if form.is_valid():
+            new_entry = form.save(commit=False)
+            new_entry.topic = topic
+            new_entry.save()
+            return redirect('learning_logs:topic', topic_id=topic_id)
+    # Display a blank or invalid form.
+    context = {'topic': topic, 'form': form}
+    return render(request, 'learning_logs/new_entry.html', context)
